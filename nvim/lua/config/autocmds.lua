087b894fa24370a8dc235a7e83c013d7ceb73bb3
Devin Andrews <devinandrews@devins-mbp.lan>
2024-02-04 00:55:50 -0500

    nvim

---
+++ nvim/lua/config/autocmds.lua
@@ -0,0 +1,24 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+
+-- TypeScript/JavaScript
+local ts_grp = vim.api.nvim_create_augroup("TSAutocmds", {})
+-- EsLintAutoFix on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = { "*.ts", "*.tsx", "*.js", "*.jsx" },
+  --command = "",
+  callback = function()
+    vim.cmd("TSToolsOrganizeImports")
+    vim.cmd("TSToolsRemoveUnusedImports")
+    vim.cmd("TSToolsAddMissingImports")
+    vim.cmd("TSToolsFixAll")
+  end,
+  group = ts_grp,
+})
+-- LspRestart eslint on saving package.json
+--vim.api.nvim_create_autocmd("BufWritePost", {
+--  pattern = { "package.json" },
+--  command = "LspRestart eslint",
+-- group = ts_grp,
+--})
---
+++ nvim/lua/plugins/tsserver.lua
@@ -0,0 +1,31 @@
+return {
+  --{ import = "lazyvim.plugins.extras.lang.typescript" },
+
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
